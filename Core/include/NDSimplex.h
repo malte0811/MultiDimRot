0dba6ffb6dc2e3d1e65b46d8ad948c0a03ff3dc5
malte0811 <malte0811@web.de>
2016-10-24 21:08:30 +0200

    MultiDimRot2.0 is licenced under GPL3 now added a working makefile removed eclipse project files

--- Core/include/NDSimplex.h
+++ Core/include/NDSimplex.h
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * This file is part of MultiDimRot2.0.
+ * Copyright (C) 2016 malte0811
+ *
+ * MultiDimRot2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MultiDimRot2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MultiDimRot2.0.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 #ifndef SRC_MULTIDIMROT_POLYTOPE_NDSIMPLEX_H_
 #define SRC_MULTIDIMROT_POLYTOPE_NDSIMPLEX_H_
 
